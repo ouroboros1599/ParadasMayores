86e14393569c4857e65e00983fbc5b2519006bcd
Jesús Castillo Morales <102334493+Ouroboros1599@users.noreply.github.com>
2023-12-13 02:18:09 -0300

    creación nuevas migraciones y nuevos modelos. Nuevo modelo de base de datos

---
+++ database/migrations/2023_12_13_051439_create_actividad_material_talento_humanos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateActividadMaterialTalentoHumanosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('actividad_material_talento_humanos', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('actividad_material_talento_humanos');
+    }
+}
