6c7cc957108d2163f186827653b69657ef08613b
Jesús Castillo Morales <102334493+Ouroboros1599@users.noreply.github.com>
2023-12-14 03:15:30 -0300

    PM_Ctrl Creación esqueleto de todos los controladores

---
+++ app/Http/Controllers/PlanificacionEmpresaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PlanificacionEmpresa;
+use Illuminate\Http\Request;
+
+class PlanificacionEmpresaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\PlanificacionEmpresa  $planificacionEmpresa
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PlanificacionEmpresa $planificacionEmpresa)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\PlanificacionEmpresa  $planificacionEmpresa
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PlanificacionEmpresa $planificacionEmpresa)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\PlanificacionEmpresa  $planificacionEmpresa
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PlanificacionEmpresa $planificacionEmpresa)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\PlanificacionEmpresa  $planificacionEmpresa
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PlanificacionEmpresa $planificacionEmpresa)
+    {
+        //
+    }
+}
