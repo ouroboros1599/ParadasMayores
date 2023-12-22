b0eff756f0fcff34363215dee9058d440401a34f
Jesús Castillo Morales <102334493+Ouroboros1599@users.noreply.github.com>
2023-12-21 22:49:03 -0300

    PM_Config Configuración de las rutas en web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 
+//Rutas hacia el controllador de cada tabla 
+use App\Http\Controllers\ActividadController;
+use App\Http\Controllers\ActividadMaterialTalentoHumanoController;
+use App\Http\Controllers\ActividadResponsableController;
+use App\Http\Controllers\CargoController;
+use App\Http\Controllers\EmpresaController;
+use App\Http\Controllers\MaterialController;
+use App\Http\Controllers\PlanificacionController;
+use App\Http\Controllers\PlanificacionEmpresaController;
+use App\Http\Controllers\ResponsableController;
+use App\Http\Controllers\RolController;
+use App\Http\Controllers\TalentoHumanoController;
+use App\Http\Controllers\UsuarioController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +29,29 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Enrutamiento 
+//Actividad
+Route::resource('actividad', ActividadController::class);
+//Nav Actividad Material TalentoHumano
+Route::resource('actividad_material_talentohumano', ActividadMaterialTalentoHumanoController::class);
+//Nav Actividad Responsable
+Route::resource('actividad_responsable', ActividadResponsableController::class);
+//Cargo
+Route::resource('cargo', CargoController::class);
+//Empresa
+Route::resource('empresa', EmpresaController::class);
+//Material
+Route::resource('material', MaterialController::class);
+//Planificación
+Route::resource('planificacion', PlanificacionController::class);
+//Nav Planificacion Empresa
+Route::resource('planificacion_empresa', PlanificacionEmpresaController::class);
+//Responsable
+Route::resource('responsable', ResponsableController::class);
+//Rol
+Route::resource('rol', RolController::class);
+//TalentoHumano
+Route::resource('talentohumano',TalentoHumanoController::class);
+//Usuario
+Route::resource('usuario', UsuarioController::class);
