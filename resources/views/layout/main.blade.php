97a559ebfb064f4891e74433d8b3b571a4deed5e
Matias Castillo <rivera.matias@hotmail.com>
2023-12-29 18:29:35 -0300

    PM_Vista Planificación - Maqueta (Frontend). Integración de imagenes. Se comenta Bootstrap js, fontawesome.

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous"/>
+    {{-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous"/> --}}
     
     <!-- Styles -->
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
@@ -14,7 +14,7 @@
     <!-- Titulo de la pagina -->
     <title>@yield('tituloPagina')</title>
 </head>
-    <body>  
+    <body id="app">  
         <header>
             <div class="header">
                 @yield('header')
